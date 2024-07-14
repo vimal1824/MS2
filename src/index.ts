302cc490999d411b0bccbb383edda7b1fcb2c6d4
vimal1824 <107545675+vimal1824@users.noreply.github.com>
2024-07-14 20:29:11 +0530

    config

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("MS2");
@@ -13,6 +14,6 @@ app.get("/ms2", (req, res) => {
   res.send("<h1>MS2</h1>");
 });
 
-app.listen(8081, () => {
-  console.log("Server is running on port 8081");
+app.listen(port, () => {
+  console.log("Server is running on port ", port);
 });
